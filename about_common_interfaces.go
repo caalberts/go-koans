5b9069d9498800a953ca76514af6927dadcdfea2
Albert Salim <albertlimca@gmail.com>
2017-12-07 04:00:02 +0800

    about common interfaces

--- about_common_interfaces.go
+++ about_common_interfaces.go
@@ -17,7 +17,7 @@ func aboutCommonInterfaces() {
 		   $ open http://localhost:8080/pkg/io/
 		   $ open http://localhost:8080/pkg/bytes/
 		*/
-
+		out.Write(in.Bytes())
 		assert(out.String() == "hello world") // get data from the io.Reader to the io.Writer
 	}
 
@@ -27,6 +27,7 @@ func aboutCommonInterfaces() {
 
 		out := new(bytes.Buffer)
 
+		out.Write(in.Next(5))
 		assert(out.String() == "hello") // duplicate only a portion of the io.Reader
 	}
 }
